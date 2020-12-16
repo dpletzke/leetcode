741259293dbb8b975d194bbca030ee4c5d7c1b24
daniel <dpletzke@gmail.com>
2020-12-16 00:50:27 -0600

    work on soltuion

--- two-sum.js
+++ two-sum.js
@@ -9,16 +9,36 @@
  * @param {number} target
  * @return {number[]}
  */
+
+
 const twoSum = function(nums, target) {
-  const comps = nums.map(n => target - n);
+  
+  const complements = nums.map(n => target - n);
 
-  const both = nums.filter(n => comps.includes(n));
+  const findComplementsNoReuse = nums.filter((num, i) => {
+    return complements.find((n, j) => num === n && i !== j );
+  });
 
-  console.log({both, comps});
-  return [nums.findIndex(n => n === both[0]), nums.findIndex(n => n === both[1])]
+  // console.log(findComplementsNoReuse);
+  const [first, second] = findComplementsNoReuse;
+  const firstIndex = nums.findIndex(n => n === first);
+  const secondIndex = nums.findIndex((n, i) => n === second && i !== firstIndex);
+
+  return [firstIndex, secondIndex];
   
 };
 
-console.log(twoSum([2,7,11,15], 9));
-console.log(twoSum([3,2,4], 6));
-console.log(twoSum([3,3], 6));
+// console.log(twoSum([11,15,5,8], 13));
+// console.log();
+// console.log(twoSum([2,7,11,15], 9));
+// console.log()
+// console.log(twoSum([3,2,4], 6));
+// console.log()
+// console.log(twoSum([3,3], 6));
+// console.log()
+console.log(twoSum([0, 4, 3, 0], 0));
+
+// const nums = 3, 2, 4
+// const comps = 3, 4, 2
+
+// const both = 3, 2, 4
