8118cab098a8473a5ec2165c49fc0b3269ec677b
daniel <dpletzke@gmail.com>
2020-12-16 22:29:55 -0600

    inital solution

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -1,28 +1,35 @@
+const replaceByRecursion = (s, reg, val) => {
+  if (!reg.test(s)) return s;
+  const buffer = s.replace(reg, val);
+  console.log(buffer);
+  return replaceByRecursion(buffer, reg, val);
+};
+
 /**
  * Returns true if all brackets have correct closing order brackets;
  * @param {string} s
  * @return {boolean}
  */
 const isValid = function(s) {
-  // even number of all brackets or zero
-  // order must be so that all internal brackets must be closable
-  // within each match verify that each bracket side has the same number
+  // recursively remove matched parens,
+  // if nothing to remove and more left, return false
 
   const parensCheck = s.match(/[(]/g)?.length === s.match(/[)]/g)?.length;
   const sqCheck = s.match(/[[]/g)?.length === s.match(/[\]]/g)?.length;
   const curCheck = s.match(/[{]/g)?.length === s.match(/[}]/g)?.length;
 
-  if (!parensCheck || !sqCheck || !curCheck) return false;
+  if (!parensCheck || !sqCheck || !curCheck) {
+    return false;
+  }
 
-  const regexParen = s.match(/[(].*[)]/g) || [];
-  const regexSq = s.match(/[[].*[\]]/g) || [];
-  const regexCur = s.match(/[{].*[}]/g) || [];
+  const replaced = replaceByRecursion(s, /((\(\))|(\[])|({}))/g, '');
 
-  return [...regexParen, ...regexSq, ...regexCur].every(m => isValid(m.slice(1, -1)));
+  return replaced.length === 0;
 };
 
 const testArr = ["()", "()[]{}", "(]", "([)]", "{[]}", "()[{()[]}()]", "{{}}"];
 
 testArr.forEach(s => {
+  console.log();
   console.log(s, isValid(s));
 });
