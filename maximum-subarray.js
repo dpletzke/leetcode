fccf47fe98af39a174c55e5226480c41a73fb944
daniel <dpletzke@gmail.com>
2020-12-19 14:50:17 -0600

    attempt on nlogn solution

--- maximum-subarray.js
+++ maximum-subarray.js
@@ -2,17 +2,48 @@
  * @param {number[]} nums
  * @return {number}
  */
-const maxSubArray = function(nums) {
-  const output = [];
-  for (let l = 0; l <= nums.length; l++) {
-    for (let i = 0; i <= nums.length; i++) {
-      if (nums[i + l] !== undefined) {
-        output.push(nums.slice(i, i + l + 1).reduce((acc, n) => acc + n));
+const maxSubArray = function (nums) {
+
+  let max = -Infinity;
+  const ledger = nums.reduce(acc => {
+    acc.push([]);
+    return acc;
+  }, []);
+
+  for (let i = 0; i < nums.length; i++) {
+
+    const num = nums[i];
+    console.log({ max, num, ledger });
+
+    if (max < num) {
+      max = num;
+    }
+    if (i >= 1) {
+      ledger[0] += num;
+      if (ledger[0] > max) {
+        max = ledger[0];
       }
     }
+    ledger[0] = num;
+
+    if (i >= 2) {
+      ledger[1][0] += num;
+      ledger[1][1] += num;
+      if (ledger[1][0] > max) max = ledger[1][0];
+      ledger.unshift();
+      ledger.push(num);
+    } if (i >= 1) {
+      ledger[1][0] += num;
+    }
+    ledger[1].push(num);
   }
-  return Math.max(...output);
+  return max;
 };
 
-console.log(maxSubArray([1, 2, 3, 4, 5]));
-console.log(maxSubArray([0]));
+console.log(maxSubArray([1, -2, 3]));
+console.log();
+console.log(maxSubArray([2, -1, 2]));
+console.log();
+console.log(maxSubArray([-2, 4, -1]));
+console.log();
+console.log(maxSubArray([-2, 4, 5]));
