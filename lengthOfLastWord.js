82ecb555ff8ec1991f606b51a4e9c6c351b24102
daniel <dpletzke@gmail.com>
2020-12-23 21:00:56 -0600

    soltuion

---
+++ lengthOfLastWord.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLastWord = function(s) {
+  const words = s.trim().split(' ').reverse();
+  console.log(words);
+  return words[0] ? words[0].length : 0;
+};
+
+// console.log(lengthOfLastWord('bill cheese kyle'));
+console.log(lengthOfLastWord(' a'));
