48c318089d39940bafe3b79308215b35f0f60269
daniel <dpletzke@gmail.com>
2021-01-10 00:37:01 -0600

    bad solution

--- intersection-of-two-linked-lists.js
+++ intersection-of-two-linked-lists.js
@@ -11,20 +11,47 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
-const getIntersectionNode = function(headA, headB) {
+const getIntersectionNode = function (headA, headB) {
+
   let a = headA;
   let b = headB;
-  let record = [];
+  const record = [];
 
   while (a || b) {
     if (a && record.includes(a)) return a;
     if (b && record.includes(b)) return b;
 
-    record.push(a);
-    record.push(b);
+    if (a) record.push(a);
+    if (b) record.push(b);
 
-    if (a.next) a = a.next;
-    if (b.next) b = b.next;
+    if (a) a = a.next;
+    if (b) b = b.next;
   }
-  
+
+
+  return record.find((x, i) => record.lastIndexOf(x) !== i) || null;
 };
+
+//  let a = headA;
+// let b = headB;
+// const record = [];
+
+// record.push(headA);
+// record.push(headB);
+
+// while (a.next || b.next) {
+
+//   if (a.next) {
+//     a = a.next;
+//     if (a && record.includes(a)) return a;
+//     record.push(a);
+//   }
+//   if (b.next) {
+//     b = b.next;
+//     if (b && record.includes(b)) return b;
+//     record.push(b);
+//   }
+// }
+// if (a && record.includes(a)) return a;
+// if (b && record.includes(b)) return b;
+// return null;
