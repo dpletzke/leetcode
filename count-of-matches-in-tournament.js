d69cdd228020caa7c5e8ceb21302ddda0e2f749a
daniel <dpletzke@gmail.com>
2021-01-21 23:09:39 -0600

    solution

---
+++ count-of-matches-in-tournament.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numberOfMatches = function (n) {
+  if (n < 2) return 0;
+  return n % 2 === 0 ? n / 2 + numberOfMatches(n / 2) : (n - 1) / 2 + numberOfMatches((n - 1) / 2 + 1);
+};
