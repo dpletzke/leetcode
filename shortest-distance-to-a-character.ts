24ef7f967f75b9ec0255792624b8d15780d4bd19
daniel <dpletzke@gmail.com>
2021-02-08 03:20:43 -0600

    cleaner code

--- shortest-distance-to-a-character.ts
+++ shortest-distance-to-a-character.ts
@@ -4,23 +4,21 @@ function shortestToChar(s: string, c: string): number[] {
     return -1;
   });
 
-  let i = 0;
-  const compare = [undefined, -1];
-  while (output.includes(-1)) {
+  for (let i = output.findIndex((e) => e === 0) + 1; i < output.length; i++) {
     if (output[i] === -1) {
-      if (!compare.includes(output[i - 1])) output[i] = output[i - 1] + 1;
-      if (!compare.includes(output[i + 1])) output[i] = output[i + 1] + 1;
+      output[i] = output[i - 1] + 1;
     }
-    i = i + 1 < output.length ? i + 1 : 0;
   }
-
-  for (let i = output.length - 2; i >= 1; i--) {
-    if (output[i] !== 0) {
-      output[i] = Math.min(output[i + 1] + 1, output[i - 1] + 1);
+  for (let i = output.length - 2; i >= 0; i--) {
+    if (![0, -1].includes(output[i])) {
+      output[i] = Math.min(output[i + 1] + 1 || 0, output[i - 1] + 1 || 0);
+    }
+    if (output[i] === -1) {
+      output[i] = output[i + 1] + 1;
     }
   }
 
   return output;
 }
 
-console.log(shortestToChar("aabaacbksglinib", "b"));
+console.log(shortestToChar("aabaacbksglinibl", "b"));
