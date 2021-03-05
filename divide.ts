ca0d137674f2d62c73a81044ceaa558455de77a9
daniel <dpletzke@gmail.com>
2021-03-05 00:52:37 -0600

    solution

--- divide.ts
+++ divide.ts
@@ -1,13 +1,19 @@
-
 function divide(dividend: number, divisor: number): number {
+  if (dividend === -2147483648 && divisor === -1) return 2147483647;
+  if (dividend === -2147483648 && divisor === 1) return -2147483648;
   let quotient = -1;
   let absDivid = Math.abs(dividend);
   const absDivis = Math.abs(divisor);
-  while(absDivid >= 0) {
-    quotient++; 
+  while (absDivid >= 0) {
+    quotient++;
     absDivid -= absDivis;
   }
-  return Math.min(2147483647, quotient * (dividend > 0 ? 1 : -1) * (divisor > 0 ? 1 : -1));
-};
 
-console.log(divide(10, 3))
+  return (
+    (2147483647 > quotient ? quotient : 2147483647) *
+    (dividend > 0 ? 1 : -1) *
+    (divisor > 0 ? 1 : -1)
+  );
+}
+
+console.log(divide(10, 3));
